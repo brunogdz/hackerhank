5e735f80607b797a0440ccf1827a6f7a09ffb3f4
Bruno Gomes <brunog.vix@gmail.com>
2023-04-14 01:14:14 -0300

    Time Conversion

---
+++ Java/TimeConversion.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String timeConversion(String s) {
+    // Write your code here
+    if(s.charAt(8)=='P'){
+
+        String portion = s.substring(0,8);
+        String[] times = portion.split(":");
+        if(times[0].matches("12")){
+            return times[0] + ":" + times[1] + ":" + times[2];
+        }
+        times[0] = String.valueOf(Integer.valueOf(times[0]) + 12);
+        return times[0] + ":" + times[1] + ":" + times[2];
+     }else{
+         String portion = s.substring(0,8);
+        String[] times = portion.split(":");
+        if(times[0].matches("12")){
+            times[0]= "00";
+            return times[0] + ":" + times[1] + ":" + times[2];
+        }
+         return s.substring(0,8);
+     }
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = Result.timeConversion(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+This repo is for uploading my answers to some problems that I'm solving while I'm learning a new language or studying a language that I used to use :) 
